e07ee05085dea1502ce9c553e0710164742942da
Douglas Evole <douglaseamutete@gmail.com>
2020-10-08 18:57:21 +0300

    modifie js script.js

--- js/js script.js
+++ js/js script.js
@@ -4,36 +4,36 @@ function checkName(){
     var day = document.getElementById("day").value;
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var doug = new Date();
-    doug.setDate(parseInt(day));
-    doug.setMonth(parseInt(Month)-1);
-    doug.setFullYear(parseInt(year));
-    var d = doug.getDay();
+    var notify = new Date();
+    notify.setDate(parseInt(day));
+    notify.setMonth(parseInt(Month)-1);
+    notify.setFullYear(parseInt(year));
+    var d = notify.getDay();
     if(d===0){
-        alert("Sunday");
+        ("Sunday");
     }else if(d===1){
-        alert("Monday");
+        ("Monday");
     }else if(d===2){
-        alert("Tuesday");
+        ("Tuesday");
     }else if(d===3){
-        alert("Wednesday");
+        ("Wednesday");
     }else if(d===4){
-        alert("Thursday");
+        ("Thursday");
     }else if(d===5){
-        alert("Friday");
+        ("Friday");
     }else if(d===6){
-        alert("Saturday");
+        ("Saturday");
     }
     if(year==""||year>2030){
-        alert("insert a valid year");
+        ("insert a valid year");
     }else if(Month=="" ||Month>12){
-        alert("Enter a valid month");
+        ("Enter a valid month");
     }else if(day=="" ||day>31){
-        alert("Enter a valid day");
+        ("Enter a valid day");
     }else if(document.getElementById("male").checked==true && document.getElementById("female").checked==false){
-        alert("Your Akan name is" + maleName[d]);
+        ("Your Akan name is" + maleName[d]);
     }else if(document.getElementById("male").checked==false && document.getElementById("female").checked==true){
-        alert("Your Akan name is" + femaleName[d]);
+        ("Your Akan name is" + femaleName[d]);
     }
 }
 
